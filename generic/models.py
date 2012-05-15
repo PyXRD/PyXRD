Fixed R1G3 Model Added more cached values for faster computation, improved certain aspects of calculation & iteration

generic/models.py
@@ -295,6 +295,10 @@ class XYData(ChildModel, Storable, Observable):
         else:
             return 0
     
+    def update_from_data(self, data_x, data_y):
+        self.xy_data.update_from_data(data_x, data_y)
+        self.update_data()
+    
     def update_data(self, silent=False):
         if len(self.xy_data._model_data_x) > 1:
             
@@ -315,7 +319,7 @@ class XYData(ChildModel, Storable, Observable):
             #    offset *= self.parent.parent.get_max_intensity()
             
             trans = transforms.Affine2D().translate(0, offset)
-            data = trans.transform(np.array(zip(data_x, data_y)))
+            data = trans.transform(np.array([data_x, data_y]).transpose())
             self.line.set_data(np.transpose(data))
             self.line.set_visible(True)
         else:

generic/treemodels.py
@@ -170,17 +170,22 @@ class ObjectListStore(_BaseObjectListStore, Storable):
     def remove(self, itr):
         self.remove_item(self.get_user_data(itr))
     def remove_item(self, item):
-        path = (self._model_data.index(item),)
-        self._model_data.remove(item)
+        self.__remove_item_index(item, self._model_data.index(item))
+    def __remove_item_index(self, item, index):
+        del self._model_data[index]
         if hasattr(item, "__list_store__"):
             item.__list_store__ = None
         self.emit('item-removed', item)
-        self.row_deleted(path)
+        self.row_deleted((index,))
 
-    def clear(self):
+    def clear(self, callback=None):
         data = list(self._model_data) #make a copy
-        for item in data:
-            self.remove_item(item)
+        def reverse_enum(L):
+           for index in reversed(xrange(len(L))):
+              yield index, L[index]
+        for i, item in reverse_enum(data):
+            self.__remove_item_index(item, i)
+            if callable(callback): callback(item)
 
     def on_item_changed(self, item):
         itr = self.create_tree_iter(item)
@@ -290,10 +295,8 @@ class IndexListStore(ObjectListStore):
         ObjectListStore.remove_item(self, item)
 
     def clear(self):
-        for item in self._model_data:
-            self._item_observer.relieve_model(item)
         self._index.clear()
-        ObjectListStore.clear(self)
+        ObjectListStore.clear(self, self._item_observer.relieve_model)
        
     def item_in_model(self, item):
         return (getattr(item, self._index_column_name) in self._index)
@@ -438,32 +441,36 @@ class XYListStore(_BaseObjectListStore, Storable):
         itr = self.get_iter(path)
         self.row_inserted(path, itr)
 
-    def remove_from_path(self, *paths):
-        paths = np.sort(paths)[::-1]
-        for path in paths:
-            index = int(path)
-            self.emit('item-removed', (self._model_data_x[index], self._model_data_y[index]))
-            self._model_data_x = np.delete(self._model_data_x, index)
-            self._model_data_y = np.delete(self._model_data_y, index)
-            self.row_deleted((index,))
+    def remove_from_index(self, *indeces):
+        if indeces != []:
+            indeces = np.sort(indeces)[::-1]
+            for index in indeces:
+                self.emit('item-removed', (self._model_data_x[index], self._model_data_y[index]))
+                self._model_data_x = np.delete(self._model_data_x, index)
+                self._model_data_y = np.delete(self._model_data_y, index)
+                self.row_deleted((index,))
 
     def remove(self, itr):
         path = self.get_path(itr)
-        self.remove_from_path(path)
+        self.remove_from_index(path[0])
 
     def clear(self):
-        self.remove_from_path(*map(lambda i: (i,), range(self._model_data_x.shape[0])))
-        #self._model_data_x = np.array([])
-        #self._model_data_y = np.array([])
-        #self.invalidate_iters()
+        if self._model_data_x.shape[0] > 0:
+            self.remove_from_index(*range(self._model_data_x.shape[0]))              
         
     def set_from_data(self, data_x, data_y):
-        self.remove_from_path(*map(lambda i: (i,), range(self._model_data_x.shape[0])))
+        self.clear()
         self._model_data_x = np.array(data_x)
         self._model_data_y = np.array(data_y)
         for pos in range(self._model_data_x.shape[0]):
             self._emit_added((pos,))
-        #self.invalidate_iters()
+        
+    def update_from_data(self, data_x, data_y):
+        if data_x.shape == self._model_data_x.shape and data_y.shape == self._model_data_y.shape:
+            self._model_data_x = data_x
+            self._model_data_y = data_y
+        else:
+            self.set_from_data(data_x, data_y)
         
     def get_raw_model_data(self):
         return self._model_data_x, self._model_data_y

mixture/models.py
@@ -193,7 +193,6 @@ class Mixture(ChildModel, ObjectListStoreChildMixin, Storable):
         #         t the number of data points for that specimen
         n, m = self.data_phase_matrix.shape
                 
-        #t1 = time.time()
         calculated = [None]*n
         experimental = [None]*n
         selectors = [None]*n
@@ -205,30 +204,24 @@ class Mixture(ChildModel, ObjectListStoreChildMixin, Storable):
             calculated[i] = calc.copy()
             experimental[i] = specimen.data_experimental_pattern.xy_data._model_data_y.copy()
             selectors[i] = specimen.get_exclusion_selector(theta_range*todeg)
-        #t2 = time.time()
-        #print '%s took %0.3f ms' % ("Getting phase intensities", (t2-t1)*1000.0)
                 
                 
         #2 optimize the fractions
-        def calculate_total_R2(fractions_and_scales):
+        def calculate_total_R2(fractions_and_scales, *args):
             tot_Rp = 0.0
-            tot_R2 = 0.0
-            
-            fractions = fractions_and_scales.copy()[:m] #first m numbers are the fractions
-            scales = fractions_and_scales.copy()[-n:] #last n numbers are the scales
-            fractions = fractions[:,np.newaxis]
+            scales = fractions_and_scales[-n:] #last n numbers are the scales
+            fractions = fractions_and_scales[:m][:,np.newaxis]
                        
             for i in range(n):
                 calc = scales[i] * np.sum(calculated[i]*fractions, axis=0)
                 exp = experimental[i][selectors[i]]
                 cal = calc[selectors[i]]
-                Rp,R2 = Statistics._calc_RpR2(exp, cal)
-                tot_Rp += abs(R2) + Rp
+                tot_Rp += Statistics._calc_Rp(exp, cal)
             return tot_Rp
         
         x0 = np.array(self.data_fractions + self.data_scales)
         bounds = [(0,None) for el in x0]
-        method = 0
+        method = 2
         lastx, lastR2 = None, None
         if method == 0: #L BFGS B
             iprint = -1 # if not settings.DEBUG else 0
@@ -236,6 +229,8 @@ class Mixture(ChildModel, ObjectListStoreChildMixin, Storable):
         elif method == 1: #SIMPLEX
             disp = 0
             lastx = scipy.optimize.fmin(calculate_total_R2, x0, disp=disp)
+        elif method == 2: #L BFGS B: FAST WIDE + SLOW NARROW
+            lastx, lastR2 = Mixture.mod_l_bfgs_b(calculate_total_R2, x0, bounds)
         
         #print info
        
@@ -254,6 +249,22 @@ class Mixture(ChildModel, ObjectListStoreChildMixin, Storable):
         if not silent: self.has_changed.emit()
         
         return lastR2   
+       
+    @staticmethod
+    def mod_l_bfgs_b(func, x0, init_bounds, args=[], f1=1e12, f2=10):
+        iprint = -1
+        lastx, lastR2, info = scipy.optimize.fmin_l_bfgs_b(func, x0, approx_grad=True, factr=f1, bounds=init_bounds, args=args, iprint=iprint)
+        lastx = np.array(lastx)        
+        lowerx = lastx*0.95
+        upperx = lastx*1.05
+        bounds = np.zeros(shape=lowerx.shape + (2,))
+        for i, (minx, maxx) in enumerate(init_bounds):
+            maxx = maxx if maxx!=None else upperx[i]
+            minx = minx if minx!=None else lowerx[i]
+            bounds[i, 0] = max(lowerx[i], minx)
+            bounds[i, 1] = min(upperx[i], maxx)
+        lastx, lastR2, info = scipy.optimize.fmin_l_bfgs_b(func, lastx, approx_grad=True, factr=f2, args=args, bounds=bounds, iprint=iprint)
+        return lastx, lastR2
        
     def update_refinement_treestore(self): #TODO add different CSDS distributions!
         unique_phases = np.unique(self.data_phase_matrix.flatten())
@@ -314,47 +325,49 @@ class Mixture(ChildModel, ObjectListStoreChildMixin, Storable):
                     while gtk.events_pending():
                         gtk.main_iteration(False)
     
+    refine_lock = False
     def refine(self, gui_callback):
-        ref_props = []
-        values = []
-        ranges = tuple()
-    
-        for ref_prop in self.data_refineables._model_data:
-            if ref_prop.refine and ref_prop.refineable:
-                ref_props.append(ref_prop)
-                values.append(ref_prop.value)
-                ranges = ranges + ((ref_prop.value_min, ref_prop.value_max),)
-               
-        original_vals = values
-        x0 = np.array(values, dtype=float)
+        if not self.refine_lock:
+            self.refine_lock = True            
+            ref_props = []
+            values = []
+            ranges = tuple()
         
-        global count
-        count = 0        
-        def refine_func(new_values, gui_callback):
-            for i, ref_prop in enumerate(ref_props):
-                if not (new_values.shape==()):
-                    ref_prop.value = new_values[i]
-                else:
-                    ref_prop.value = new_values[()]
-            R = self.optimize(silent=True)
-              
-            global count
-            count = count + 1
-            if count>=5:
-                count = 0
-                if gui_callback!=None: gui_callback(R)
-                while gtk.events_pending():
-                    gtk.main_iteration(False)
-            return R
+            self.parent.freeze_updates()
+        
+            for ref_prop in self.data_refineables._model_data:
+                if ref_prop.refine and ref_prop.refineable:
+                    ref_props.append(ref_prop)
+                    values.append(ref_prop.value)
+                    ranges = ranges + ((ref_prop.value_min, ref_prop.value_max),)
+                   
+            original_vals = values
+            x0 = np.array(values, dtype=float)
             
-        #1. First brute-force solve the problem:
-        #x0, fval, grid, Jout = scipy.optimize.brute(refine_func, ranges, args=[None,], Ns=5, full_output=True)
-        #print "Brute Force Solution:"
-        #print x0
-        
-        #2. Then really refine this brute force solution using l_bfgs_b algorithm:
-        iprint = -1 # if not settings.DEBUG else 0
-        lastx, lastR2, info = scipy.optimize.fmin_l_bfgs_b(refine_func, x0, args=[gui_callback,], approx_grad=True, factr=1e12, bounds=ranges, iprint=iprint)
+            global count
+            count = 0        
+            def refine_func(new_values, gui_callback):
+                for i, ref_prop in enumerate(ref_props):
+                    if not (new_values.shape==()):
+                        ref_prop.value = new_values[i]
+                    else:
+                        ref_prop.value = new_values[()]
+                R = self.optimize(silent=True)
+                  
+                global count
+                count = count + 1
+                if count>=5:
+                    count = 0
+                    if gui_callback!=None: gui_callback(R)
+                    while gtk.events_pending():
+                        gtk.main_iteration(False)
+                return R
+                
+            lastx, lastR2 = Mixture.mod_l_bfgs_b(refine_func, x0, ranges, args=[gui_callback,], f2=1e6)
+            
+            self.refine_lock = False
+            self.parent.thaw_updates()
+            
         
                     
     def apply_result(self):

phases/models.py
@@ -23,6 +23,32 @@ from generic.treemodels import ObjectListStore
 from atoms.models import Atom
 from probabilities.models import get_correct_probability_model
 
+
+
+# TODO MOVE THIS SOMEWHERE ELSE
+from numpy.core.umath_tests import matrix_multiply as mmultr
+def mmult(A, B):
+    return np.sum(np.transpose(A,(0,2,1))[:,:,:,np.newaxis]*B[:,:,np.newaxis,:],-3)
+def mdot(A,B):
+    C = np.zeros(shape=A.shape, dtype=np.complex)
+    for i in range(A.shape[0]):
+        C[i] = np.dot(A[i], B[i])
+    return C
+def mtim(A,B):
+    C = np.zeros(shape=A.shape, dtype=np.complex)
+    for i in range(A.shape[0]):
+        C[i] = np.multiply(A[i], B[i])
+    return C
+        
+    
+def solve_division(A,B):
+    bt = np.transpose(B, axes=(0,2,1))
+    at = np.transpose(A, axes=(0,2,1))
+    return np.array([np.transpose(np.linalg.lstsq(bt[i], at[i])[0]) for i in range(bt.shape[0])])
+#END TODO
+
+
+
 class Component(ChildModel, ObjectListStoreChildMixin, Storable):
 
     #MODEL INTEL:
@@ -53,9 +79,12 @@ class Component(ChildModel, ObjectListStoreChildMixin, Storable):
     data_name = "Name of this component"
     
     _dirty = True
-    def get_dirty_value(self): return self._dirty
+    def get_dirty_value(self): return (self._dirty)
     def set_dirty_value(self, value):
-        if value!=self._dirty: self._dirty = value
+        if value!=self._dirty: 
+            self._dirty = value
+            if self._dirty:
+                self._cached_factors = dict()        
     
     _inherit_cell_a = False
     _inherit_cell_b = False
@@ -117,8 +146,9 @@ class Component(ChildModel, ObjectListStoreChildMixin, Storable):
         ChildModel.__init__(self, parent=parent)
         Storable.__init__(self)
         self.needs_update = Signal()
-        self._dirty = True
-        
+        self.dirty = True
+        self._cached_factors = dict()
+                
         self.data_name = data_name or self.data_name
     
         self._data_d001 = data_d001 or self.data_d001
@@ -155,6 +185,7 @@ class Component(ChildModel, ObjectListStoreChildMixin, Storable):
     def notify_dirty_changed(self, model, prop_name, info):
         if model.dirty: self.dirty = True
         pass
+
     
     # ------------------------------------------------------------
     #      Input/Output stuff
@@ -183,16 +214,18 @@ class Component(ChildModel, ObjectListStoreChildMixin, Storable):
     # ------------------------------------------------------------
     #      Methods & Functions
     # ------------------------------------------------------------  
-    def get_structure_factors(self, range_stl):
-        sf_tot = np.zeros(range_stl.shape, dtype=np.complex_)
-        atoms = (self.data_layer_atoms._model_data + self.data_interlayer_atoms._model_data)
-        for atom in atoms:
-            sf_tot += atom.get_structure_factors(range_stl)
-        return sf_tot
-
-    def get_phase_factors(self, range_stl):
-        return np.exp(2*pi*self.data_d001*range_stl*1j)
-
+    def get_factors(self, range_stl):
+        hsh = get_md5_hash(range_stl)
+        if self.dirty or not hsh in self._cached_factors:
+            sf_tot = np.zeros(range_stl.shape, dtype=np.complex_)
+            for atom in self.data_layer_atoms._model_data:
+                sf_tot += atom.get_structure_factors(range_stl)
+            for atom in self.data_interlayer_atoms._model_data:
+                sf_tot += atom.get_structure_factors(range_stl)
+            self._cached_factors[hsh] = sf_tot, np.exp(2*pi*self.data_d001*range_stl*1j)
+            self.dirty = False
+        return self._cached_factors[hsh]
+        
     def get_cell_a(self):
         return self.data_cell_a
         
@@ -249,9 +282,8 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
     def set_dirty_value(self, value):
         if value!=self._dirty:
             self._dirty = value
-            self._cached_diffracted_intensities = dict()
-
-    
+            if self._dirty: self._cached_diffracted_intensities = dict()
+   
     _inherit_mean_CSDS = False
     _inherit_min_CSDS = False
     _inherit_max_CSDS = False
@@ -264,23 +296,6 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
         setattr(self, "_%s" % prop_name, value)
         self.dirty = True
         self.needs_update.emit()
-    
-    based_on_observer = None
-    """class BasedOnObserver(Observer): #FIXME MOVE THIS DOWN!
-        phase = None
-        
-        def __init__(self, phase, *args, **kwargs):
-            self.phase = phase
-            Observer.__init__(self, *args, **kwargs)
-        
-        @Observer.observe("removed", signal=True)
-        def notification(self, model, prop_name, info):
-            #model      = phase that we're observing
-            #self.phase = phase that is observing the model
-            if model == self.phase.data_based_on: #this check is not 100% neccesary, but it can't hurt either
-                self.phase.data_based_on = None
-                for component in self.phase.data_components:
-                    component.data_linked_with = None"""
                 
     _based_on_index = None #temporary property
     _data_based_on = None
@@ -358,7 +373,7 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
         ChildModel.__init__(self, parent=parent)
         Storable.__init__(self)
         self._dirty = True
-        self._cached_diffracted_intensities = dict()
+        self._cached_diffracted_intensities = dict()  
         
         self.needs_update = Signal()
         
@@ -527,36 +542,24 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
             
         return ifs"""
        
-    #@print_timing
+    _cached_CSDS_matrices = None
+    def get_CSDS_matrices(self, Q):
+        Qn = np.empty((self.data_max_CSDS+1,), dtype=object)
+        Qn[1] = np.copy(Q)
+        for n in range(2, int(self.data_max_CSDS+1)):
+            Qn[n] = mmult(Qn[n-1], Q)
+        return Qn
+
+       
     _cached_diffracted_intensities = None
-    def get_diffracted_intensity (self, range_theta, range_stl, lpf_callback, quantity, correction_range):
+    def get_diffracted_intensity (self, range_theta, range_stl, lpf_callback, quantity, correction_range): #TODO CACHE CACHE
         hsh = get_md5_hash(range_theta)
         if self.dirty or not hsh in self._cached_diffracted_intensities:
             #print "for specimen %s" % self
-            from numpy.core.umath_tests import matrix_multiply as mmultr
-            def mmult(A, B):
-                return np.sum(np.transpose(A,(0,2,1))[:,:,:,np.newaxis]*B[:,:,np.newaxis,:],-3)
-            def mdot(A,B):
-                C = np.zeros(shape=A.shape, dtype=np.complex)
-                for i in range(A.shape[0]):
-                    C[i] = np.dot(A[i], B[i])
-                return C
-            def mtim(A,B):
-                C = np.zeros(shape=A.shape, dtype=np.complex)
-                for i in range(A.shape[0]):
-                    C[i] = np.multiply(A[i], B[i])
-                return C
-                    
-                
-            def solve_division(A,B):
-                bt = np.transpose(B, axes=(0,2,1))
-                at = np.transpose(A, axes=(0,2,1))
-                return np.array([np.transpose(np.linalg.lstsq(bt[i], at[i])[0]) for i in range(bt.shape[0])])
-
+            
             stl_dim = range_stl.shape[0]
-            def repeat_to_stl(arr):
-                return np.repeat(arr[np.newaxis,...], stl_dim, axis=0)
-           
+            repeat_to_stl = lambda arr: np.repeat(arr[np.newaxis,...], stl_dim, axis=0)
+            
             #Get interference (log-normal) distribution:
             distr, ddict, real_mean = self._update_interference_distributions()
             
@@ -566,7 +569,7 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
             W = repeat_to_stl(W).astype(np.complex_)
             P = repeat_to_stl(P).astype(np.complex_)
             G = self.data_G
-            
+
             #get structure factors and phase factors for individual components:
             #        components
             #       .  .  .  .  .  .
@@ -576,17 +579,16 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
             shape = range_stl.shape + (G,)
             SF = np.zeros(shape, dtype=np.complex_)
             PF = np.zeros(shape, dtype=np.complex_)
+            t1 = time.time() #FIXME
             for i, component in enumerate(self.data_components._model_data):
-                SF[:,i] = component.get_structure_factors(range_stl)
-                PF[:,i] = component.get_phase_factors(range_stl)
-                component.dirty = False
-            
+                SF[:,i], PF[:,i] = component.get_factors(range_stl)
+            t2 = time.time() #FIXME
             intensity = np.zeros(range_stl.size, dtype=np.complex_)
             first = True
 
             rank = P.shape[1]
             reps = rank / G
-             
+                        
             #Create Phi & F matrices:        
             SFa = np.repeat(SF[...,np.newaxis,:], SF.shape[1], axis=1)
             SFb = np.transpose(np.conjugate(SFa), axes=(0,2,1)) #np.conjugate(np.repeat(SF[...,np.newaxis], SF.shape[1], axis=2)) 
@@ -599,15 +601,13 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
                               
             #Calculate the intensity:
             method = 0
+
+            t3 = time.time() #FIXME
+            Qn = self.get_CSDS_matrices(Q)
+            t4 = time.time() #FIXME
                 
             if method == 0:
                 ################### FIRST WAY ###################                 
-                
-                Qn = np.empty((self.data_max_CSDS+1,), dtype=object)
-                Qn[1] = np.copy(Q)
-                for n in range(2, int(self.data_max_CSDS+1)):
-                    Qn[n] = mmult(Qn[n-1], Q)
-                      
                 SubTotal = np.zeros(Q.shape, dtype=np.complex)
                 CSDS_I = repeat_to_stl(np.identity(rank, dtype=np.complex) * real_mean)
                 for n in range(1, int(self.data_max_CSDS)+1):
@@ -624,11 +624,6 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
                 I = repeat_to_stl(np.identity(rank))
                 CSDS_I = repeat_to_stl(np.identity(rank, dtype=np.complex_) * real_mean)
                       
-                Qn = np.empty((self.data_max_CSDS+1,), dtype=object)
-                Qn[1] = np.copy(Q)
-                for n in range(2, int(self.data_max_CSDS+1)):
-                    Qn[n] = mmult(Qn[n-1], Q)
-                      
                 IQ = (I-Q)
                 IIQ = solve_division(I, IQ)
                 IIQ2 = solve_division(I, mmult(IQ,IQ))
@@ -637,10 +632,17 @@ class Phase(ChildModel, ObjectListStoreChildMixin, Storable):
                     R = (I + 2*Q*IIQ + (2 / n) * (Qn[n+1]-Q) * IIQ2) * ddict[n]
                     intensity += np.real(np.trace(mmult(mmult(F, W), R), axis1=2, axis2=1))
                 
+            t5 = time.time() #FIXME    
             lpf = lpf_callback(range_theta, self.data_sigma_star)
+            t6 = time.time() #FIXME
+            
             scale = self.get_absolute_scale() * quantity
             self.dirty = False
             self._cached_diffracted_intensities[hsh] = intensity * correction_range * scale * lpf
+            
+            t7 = time.time()
+            #print '%s took %0.3f ; %0.3f ; %0.3f ; %0.3f ; %0.3f ; %0.3f' % ("get_phase_intensities", (t2-t1)*1000.0, (t3-t2)*1000.0, (t4-t3)*1000.0, (t5-t4)*1000.0, (t6-t5)*1000.0, (t7-t6)*1000.0)
+
         return self._cached_diffracted_intensities[hsh]
 
     def get_absolute_scale(self):

probabilities/models.py
@@ -316,44 +316,13 @@ class R1G3Model(_AbstractR0R1Model):
 	"""
 	Reichweite = 1 / Components = 3
 	g*(g-1) independent variables = 6
-	W0 & P00 (W0<0,5) of P11 (W0>0,5)
-	W1/(W2+W1) = G1
-	
-	(W11+W12) / (W11+W12+W21+W22) = G2
+    W0 & P00 (W0<0,5) of P11 (W0>0,5)
+    W1/(W2+W1) = G1
+    
+    (W11+W12) / (W11+W12+W21+W22) = G2
 
-	W11/(W11+W12) = G3
-	W21/(W21+W22) = G4
-	
-	W1 = (1 – W0) * W1 / (W1+W2) = (1 – W0) * G1
-	W2 = 1 – W0 – W1
-	
-	Als W0 < 0,5 (P00 gegeven):
-		W00 = P00*W0
-		
-		W11 = (W0-W1-W2-W00) * G3 * G2
-		P11 = W11 / W1
-		
-	Als W0 > 0,5 (P11 gegeven):
-		W11 = P11*W1
-
-	W12 = W11 * (1-G3)
-	P12 = W12/W1 (= P11 * (1–G3))
-	
-	W10 = W1 – W12 – W11 (= W1 – W11/G3)
-	P10 = W10/W1
-	
-	W21 = G4 * (1-G2)*(W11+W12)
-	P21 = W21/W2
-
-	W22 = (1 – G4) * W21
-	P22 = W22/W2
-	
-	W20 = W2 – W21 - W22
-	P20 = W20/W2
-	
-	P00 = 1 – P10 – P20
-	P01 = 1 – P11 – P21
-	P02 = 1 – P21 – P22
+    W11/(W11+W12) = G3
+    W21/(W21+W22) = G4
 		
 	indexes are NOT zero-based in external property names!
 	"""
@@ -362,10 +331,10 @@ class R1G3Model(_AbstractR0R1Model):
     __independent_label_map__ = [
         ("W1", "W<sub>1</sub>"),
         ("P11_or_P22", "P<sub>11</sub> or P<sub>22</sub>"),
-        ("G1", "W<sub>2</sub> / (W<sub>3</sub> + W<sub>2</sub>)"),
-        ("G2", "(W<sub>22</sub> + W<sub>23</sub>) / (W<sub>22</sub> + W<sub>23</sub> + W<sub>32</sub> + W<sub>33</sub>)"),
-        ("G3", "W<sub>22</sub> / (W<sub>22</sub> + W<sub>23</sub>)"),
-        ("G4", "W<sub>32</sub> / (W<sub>32</sub> + W<sub>33</sub>)"),
+        ("G1", "W<sub>2</sub>\n<s>             </s>\n(W<sub>3</sub> + W<sub>2</sub>)"),
+        ("G2", "(W<sub>22</sub>+W<sub>23</sub>)\n<s>                       </s>\n(W<sub>22</sub>+W<sub>23</sub>+W<sub>32</sub>+W<sub>33</sub>)"),
+        ("G3", "W<sub>22</sub>\n<s>               </s>\n(W<sub>22</sub> + W<sub>23</sub>)"),
+        ("G4", "W<sub>32</sub>\n<s>               </s>\n(W<sub>32</sub> + W<sub>33</sub>)"),  
     ]
     __refineables__ = [ prop for prop, lbl in  __independent_label_map__ ]
     __observables__ = __refineables__
@@ -410,12 +379,16 @@ class R1G3Model(_AbstractR0R1Model):
     # ------------------------------------------------------------
     #      Initialisation and other internals
     # ------------------------------------------------------------       
-    def setup(self, W1=0.25, P11_or_P22=0.5):
+    def setup(self, W1=0.6, P11_or_P22=0.3, G1=0.5, G2=0.2, G3=0.5, G4=0.2):
         self._R = 0
         self._W = np.zeros(shape=(3), dtype=float)
         self._P = np.zeros(shape=(3, 3), dtype=float)
         self.W1 = W1
         self.P11_or_P22 = P11_or_P22
+        self.G1 = G1
+        self.G2 = G2
+        self.G3 = G3
+        self.G4 = G4                
 
     # ------------------------------------------------------------
     #      Methods & Functions
@@ -424,37 +397,39 @@ class R1G3Model(_AbstractR0R1Model):
     def update(self):    
         #temporary storage:
         WW = np.matrix(np.zeros(shape=(3,3), dtype=float))
+        
+        G2inv =  ( 1.0 / self._G2) - 1.0 if self._G2 > 0 else 0.0
+        G3inv =  ( 1.0 / self._G3) - 1.0 if self._G3 > 0 else 0.0
+        G4inv =  ( 1.0 / self._G4) - 1.0 if self._G4 > 0 else 0.0
+        
              
-        self._W[1]   = (1.0 - self._W[0]) * self._G1
+        self._W[1] = (1-self._W[0]) * self._G1
     	self._W[2]   = 1.0 - self._W[0] - self._W[1]
-	
-	    if self._W[0] <= 0.5:
-	        WW[0,0]         = self._P[0,0]*self._W[0]
-	        
-	        WW[1,1]         = (self._W[0] - self._W[1] - self._W[2] - WW[0,0]) * self._G3 * self._G2
-	        self._P[1,1]    = WW[1,1] / self._W[1]
-	    else:
-	        WW[1,1]         = self._P[1,1]*self._W[1]
-	        
-        WW[1,2]         = WW[1,1] * (1.0-self._G3)
-        self._P[1,2]    = WW[1,2] / self._W[1]
+    	
+    	if self._W[0] < 0.5:
+    	    self._P[1,1] =  self._G2 * self._G3 * (self._W[0]*(self._P[0,0]-1) + self._W[1] + self._W[2]) / self._W[1]
+    	
+    	WW[1,1] = self._P[1,1] * self._W[1]
+    	WW[1,2] = WW[1,1] * G3inv
+    	WW[2,1] = self._G4 * G2inv * (WW[1,1] + WW[2,1])
+    	WW[2,2] = G4inv * WW[2,1]
+    	
+    	self._P[1,2] = WW[1,2] / self._W[1]
+    	self._P[1,0] = 1 - self._P[1,1] - self._P[1,2]
+    	
+    	self._P[2,1] = WW[2,1] / self._W[2]
+    	self._P[2,2] = WW[2,2] / self._W[2]
+    	self._P[2,0] = 1 - self._P[2,1] - self._P[2,2]
 
-        WW[1,0]         = self._W[1] - WW[1,2] - WW[1,1]
-        self._P[1,0]    = WW[1,0] / self._W[1]        
-		
-		WW[2,1]         = self._G4 * (1.0 - self._G2) * (WW[1,1] + WW[1,2])
-		self._P[2,1]    = WW[2,1] / self._W[2]
-
-		WW[2,2]         = (1.0 - self._G4) * WW[2,1]
-		self._P[2,2]    = WW[2,2]/self._W[2]
-		
-		WW[2,0]         = self._W[2] - WW[2,1] - WW[2,2]
-		self._P[2,0]    = WW[2,0]/self._W[2]
-		
-		self._P[0,0]    = 1.0 - self._P[1,0] - self._P[2,0]
-		self._P[0,1]    = 1.0 - self._P[1,1] - self._P[2,1]
-		self._P[0,2]    = 1.0 - self._P[2,1] - self._P[2,2]
-    
+    	self._P[0,1] = (self._W[1] - WW[1,1] - WW[2,1]) / self._W[0]
+    	self._P[0,2] = (self._W[2] - WW[1,2] - WW[2,2]) / self._W[0]
+    	if self._W[0] >= 0.5:
+        	self._P[0,0] = 1 - self._P[0,1] - self._P[0,2]
+        
+        for i in range(3):
+            for j in range(3):
+                WW[i,j] = self._W[i] * self._P[i,j]
+                
         self.updated.emit()
     
     pass #end of class

specimen/models.py
@@ -351,45 +351,40 @@ class Specimen(ChildModel, ObjectListStoreChildMixin, Observable, Storable):
 
     def get_phase_intensities(self, phases, lpf_callback, steps=2500):
         if phases!=None:
-            #todo part of these things should be calculated in the Goniometer and only changed when needed (e.g. the normal range)
+        
             l = self.parent.data_goniometer.data_lambda
             L_Rta =  self.data_sample_length / (self.parent.data_goniometer.data_radius * tan(radians(self.parent.data_goniometer.data_divergence)))
             min_theta = radians(self.parent.data_goniometer.data_min_2theta*0.5)
             max_theta = radians(self.parent.data_goniometer.data_max_2theta*0.5)
             delta_theta = float(max_theta - min_theta) / float(steps-1)
-            
             theta_range = None
             torad = pi / 180.0
             if self.data_experimental_pattern.xy_data._model_data_x.size <= 1:
                 theta_range = min_theta + delta_theta * np.array(range(0,steps-1), dtype=float)
             else:
-                theta_range =  self.data_experimental_pattern.xy_data._model_data_x * torad / 2
+                theta_range =  self.data_experimental_pattern.xy_data._model_data_x * torad * 0.5
             stl_range = 2 * np.sin(theta_range) / l
             
-            #sample length correction array:
-            correction_range =  np.minimum(np.sin(theta_range) * L_Rta, 1)
-            
+            correction_range = np.minimum(np.sin(theta_range) * L_Rta, 1)
+
             return theta_range, np.array([phase.get_diffracted_intensity(theta_range, stl_range, lpf_callback, 1.0, correction_range) if phase else np.zeros(shape=theta_range.shape) for phase in phases], dtype=np.float_)
 
 
-    # declare the @ decorator just before the function, invokes print_timing()
     #@print_timing
     def calculate_pattern(self, lpf_callback, steps=2500):
         if len(self._data_phases) == 0:
             self.data_calculated_pattern.xy_data.clear()
             return None
-        else:           
+        else:     
+                    
             theta_range, intensities = self.get_phase_intensities(self._data_phases.keys(), lpf_callback, steps=steps)
             intensity_range = np.zeros(len(intensities[0]))
             
             fractions = np.array(self._data_phases.values())[:,np.newaxis]
             intensity_range = np.sum(intensities*fractions, axis=0) * self.data_abs_scale
+            theta_range = theta_range * 360.0 / pi 
             
-            self.data_calculated_pattern.clear(update=False) #FIXME this should be done in ONE single call instead of 3 (see further)
-            
-            theta_range = theta_range * 2 * 180.0 / pi 
-            self.data_calculated_pattern.xy_data.set_from_data(theta_range, intensity_range)
-            self.data_calculated_pattern.update_data()
+            self.data_calculated_pattern.update_from_data(theta_range, intensity_range)
 
             return (theta_range, intensity_range)
         
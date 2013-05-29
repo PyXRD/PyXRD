eb66c892734b05a2dfc0800d87dd06b88152a0db
Mathijs Dumon <mathijs.dumon@gmail.com>
2013-05-29 12:11:32 +0200

    Added xrd parsers and file parsers unit tests

---
+++ tests/test_generic/test_io/test_xrd_parsers/test_brk_raw_parser.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding=UTF-8
+# ex:ts=4:sw=4:et=on
+
+# Copyright (c) 2013, Mathijs Dumon
+# All rights reserved.
+# Complete license can be found in the LICENSE file.
+
+import unittest
+
+from tests.test_generic.test_io.test_file_parsers import TestParserMixin, load_data_from_files
+from generic.io.xrd_parsers import BrkRAWParser
+
+
+__all__ = [
+    'TestRAWParser',
+]
+
+class TestRAWParser(TestParserMixin, unittest.TestCase):
+
+    parser_class = BrkRAWParser
+    file_data = load_data_from_files(
+        "tests/test_generic/test_io/test_xrd_parsers/brk_raw1.raw", 
+        "tests/test_generic/test_io/test_xrd_parsers/brk_raw2.raw"
+    )
+        
+    pass #end of class

BUGFIX: Change the way residuals are calculated for mixtures, these are now averaged over the number of specimens. Should ideally return multiple residuals. BUGFIX: faulty CSDS average values leads to errors, model now checks if passed value is a valid float, and if not ignores the change FEATURE: Mixture legend plotting now has matching phase color boxes next to the names REGRESSION/BUGFIX: Patterns for phases are now correctly scaled when displayed BUGFIX: default goniometer ranges are now correctly calculated (last data point was missing before) BUGFIX: CSDS data object was causing some issues when deep copied due to inproper initialisation.

goniometer/models.py
@@ -136,8 +136,8 @@ class Goniometer(ChildModel, Storable):
                 return val
         min_theta = torad(self.min_2theta*0.5)
         max_theta = torad(self.max_2theta*0.5)
-        delta_theta = float(max_theta - min_theta) / float(self.steps-1)
-        theta_range = (min_theta + delta_theta * np.arange(0,self.steps-1, dtype=float))
+        delta_theta = float(max_theta - min_theta) / float(self.steps)
+        theta_range = (min_theta + delta_theta * np.arange(0,self.steps, dtype=float)) + delta_theta*0.5
         return theta_range       
     
     def get_machine_correction_range(self, range_theta, sample_length, absorption):

mixture/models/mixture.py
@@ -297,7 +297,7 @@ class Mixture(ChildModel, ObjectListStoreChildMixin, Storable):
                 if specimen!=None:
                     specimen.update_pattern(
                         specimen_data.total_intensity,
-                        specimen_data.phase_intensities,
+                        specimen_data.phase_intensities * self.fractions[:,np.newaxis] * self.scales[i],
                         self.phase_matrix[i,:]
                     )
             
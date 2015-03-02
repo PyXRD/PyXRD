Fixing stale 'func' calls due to removal of calculation caching

pyxrd/calculations/phases.py
@@ -32,7 +32,7 @@ def get_structure_factors(range_stl, G, comp_list):
     SF = np.zeros(shape, dtype=np.complex_)
     PF = np.zeros(shape, dtype=np.complex_)
     for i, comp in enumerate(comp_list):
-        SF[:, i], PF[:, i] = get_factors.func(range_stl, comp) # @UndefinedVariable
+        SF[:, i], PF[:, i] = get_factors(range_stl, comp) # @UndefinedVariable
     return SF, PF
 
 def get_Q_matrices(Q, CSDS_max):
@@ -84,7 +84,7 @@ def get_diffracted_intensity(range_stl, phase):
         P = repeat_to_stl(phase.P).astype(np.complex_)
 
         # Repeat & get SFa and SFb (transpose conjugate) structure factor matrices:
-        SF, PF = get_structure_factors.func(range_stl, phase.G, phase.components)
+        SF, PF = get_structure_factors(range_stl, phase.G, phase.components)
         SFa = np.repeat(SF[..., np.newaxis, :], SF.shape[1], axis=1)
         SFb = np.transpose(np.conjugate(SFa), axes=(0, 2, 1))
 
@@ -98,7 +98,7 @@ def get_diffracted_intensity(range_stl, phase):
         # Create Q phase factor matrices:
         PF = np.repeat(PF[..., np.newaxis, :], PF.shape[1], axis=1)
         Q = np.multiply(np.repeat(np.repeat(PF, reps, axis=2), reps, axis=1), P)
-        Qn = get_Q_matrices.func(Q, phase.CSDS.maximum)
+        Qn = get_Q_matrices(Q, phase.CSDS.maximum)
 
         # Calculate the intensity:
         sub_total = np.zeros(Q.shape, dtype=np.complex)

pyxrd/calculations/specimen.py
@@ -33,7 +33,7 @@ def calculate_phase_intensities(specimen):
     """
     range_stl = 2 * np.sin(specimen.range_theta) / specimen.goniometer.wavelength
 
-    correction_range = get_machine_correction_range.func(specimen)
+    correction_range = get_machine_correction_range(specimen)
 
     return correction_range, correction_range * np.array([
         get_intensity(
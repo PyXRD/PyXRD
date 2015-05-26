7d2d70afc1f347b30cc9d81eb3b1042b1616d3bd
Mathijs Dumon <mathijs.dumon@gmail.com>
2015-05-26 14:21:47 +0200

    Bugfix for R1 models

--- pyxrd/probabilities/models/R1models.py
+++ pyxrd/probabilities/models/R1models.py
@@ -140,7 +140,8 @@ class R1G3Model(_AbstractProbability):
         
         \begin{align*}
                 & W_1
-                & \text{$P_{11} (W_1 < 0.5)$ or $P_{22} (W_1 > 0.5)$} \\
+                & \text{$P_{11} (W_1 < 0.5)$ or $P_{xx} (W_1 > 0.5)$}
+                with P_{xx} = \frac {W_{22} + W_{23} + W_{32} + W_{33} + W_{42} + W_{43}}{W_2 + W_3} \\
                 & G_1 = \frac{W_2}{W_2 + W_3}
                 & G_2 = \frac{W_{22} + W_{23}}{W_{22} + W_{23} + W_{32} + W_{33}} \\
                 & G_3 = \frac{W_{22}}{W_{22} + W_{23}}
@@ -148,6 +149,8 @@ class R1G3Model(_AbstractProbability):
         \end{align*}
             
     Calculation of the other variables happens as follows:
+    
+    
     
     .. math::
         :nowrap:
@@ -165,24 +168,16 @@ class R1G3Model(_AbstractProbability):
             & \quad W_3 = 1.0 - W_1 - W_2 \\
             & \\
             & \text{if $W_1 \leq 0.5$:} \\
-            & \quad \text{$P_{11}$ is given}\\
-            & \quad P_{22} =
-            \begin{dcases}
-                G_{2} \cdot G_{3} \cdot {\frac{W_1 \cdot (P_{11} - 1) + W_2 + W_3}{W_2}}, & \text{if $W_2 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
+            & \quad \text{$P_{11}$ is given and W_xx is derived as} \\
+            & \quad W_{xx} = W_{22} + W_{23} + W_{32} + W_{23} = W_1 \cdot (1 - P_{11}) + W_2 + W_3 \\ 
             & \\
             & \text{if $W_1 > 0.5$:} \\
-            & \quad \text{$P_{22}$ is given and $P_{11}$ is derived further down} \\
+            & \quad \text{$P_{xx}$ is given and $P_{11}$ is derived further down} \\
+            & \quad W_{xx} = W_{22} + W_{23} + W_{32} + W_{23} = P_{xx} \cdot (W_2 + W_3) \\
             & \\
-            & W_{22} = P_{22} \cdot W_2 \\
+            & W_{22} = W_{xx} \cdot G_2 \cdot G_3 \\
             & W_{23} = W_{22} \cdot G_3^{-1} \\
-            & \\
-            & W_{32} = {G_4 \cdot G_2^{-1}} \cdot {(W_{22} + W_{23})} \\
-            & W_{33} = G_4^{-1} \cdot W_{21} \\
-            & \\
-            & W_{23} = W_{22} * G_3^{-1} \\
-            & W_{32} = {G_4 \cdot G_2} \cdot {(W_{22} + W_{23})} \\
+            & W_{32} = W_{xx} \cdot (1 - G_2) \\
             & W_{33} = G_4^{-1} \cdot W_{32} \\
             & \\
             & P_{23} = 
@@ -231,11 +226,11 @@ class R1G3Model(_AbstractProbability):
                 stor_name="_W1", inh_name="inherit_W1", inh_from="parent.based_on.probabilities",
                 is_independent=True, # flag for the view creation
                 minimum=0.0, maximum=1.0, data_type=float, **PropIntel.REF_ST_WID),
-            PropIntel(name="P11_or_P22", label="P11_or_P22",
+            PropIntel(name="P11_or_P22", label="P11 or Pxx",
                 stor_name="_P11_or_P22", inh_name="inherit_P11_or_P22",
                 inh_from="parent.based_on.probabilities",
                 is_independent=True, # flag for the view creation
-                math_label=r"$P_{11} %s$ or $\newline P_{22} %s$" % (
+                math_label=r"$P_{11} %s$ or $\newline P_{xx} %s$" % (
                     mt_range(0.0, "W_1", 0.5),
                     mt_range(0.5, "W_1", 1.0)),
                 minimum=0.0, maximum=1.0, data_type=float, **PropIntel.REF_ST_WID),
@@ -312,28 +307,28 @@ class R1G3Model(_AbstractProbability):
     # ------------------------------------------------------------
     def update(self):
         with self.data_changed.hold_and_emit():
-            G2inv = (1.0 / self.G2) - 1.0 if self.G2 > 0.0 else 0.0
-            G3inv = (1.0 / self.G3) - 1.0 if self.G3 > 0.0 else 0.0
-            G4inv = (1.0 / self.G4) - 1.0 if self.G4 > 0.0 else 0.0
-
             self.mW[0] = self.W1
             self.mW[1] = (1 - self.mW[0]) * self.G1
             self.mW[2] = 1.0 - self.mW[0] - self.mW[1]
 
+            W0inv = 1.0 / self.mW[0] if self.mW[0] > 0.0 else 0.0
+
+            Wxx = 0
             if self.mW[0] <= 0.5: # P00 given
                 self.mP[0, 0] = self.P11_or_P22
-                if self.mW[1] > 0.0:
-                    self.mP[1, 1] = self.G2 * self.G3 * (self.mW[0] * (self.mP[0, 0] - 1.0) + self.mW[1] + self.mW[2]) / self.mW[1]
-                else:
-                    self.mP[1, 1] = 0.0
-            else: # P11 given
-                self.mP[0, 0] = 0.0 # mP[0,0] (aka P11) is derived further down
-                self.mP[1, 1] = self.P11_or_P22
+                # Wxx = W11 + W12 + W21 + W22
+                Wxx = self.mW[0] * (self.mP[0, 0] - 1) + self.mW[1] + self.mW[2]
+            else: # Pxx given
+                # Wxx = W11 + W12 + W21 + W22
+                Wxx = (1.0 - self.mW[0]) * self.P11_or_P22
 
-            self.mW[1, 1] = self.mP[1, 1] * self.mW[1]
-            self.mW[1, 2] = self.mW[1, 1] * G3inv
-            self.mW[2, 1] = self.G4 * G2inv * (self.mW[1, 1] + self.mW[1, 2])
-            self.mW[2, 2] = G4inv * self.mW[2, 1]
+            # W11 + W12 = Wxx * G2:
+            self.mW[1, 1] = Wxx * self.G2 * self.G3
+            self.mW[1, 2] = Wxx * self.G2 * (1 - self.G3)
+            self.mP[1, 1] = self.mW[1, 1] / self.mW[1] if self.mW[1] > 0.0 else 0.0
+
+            self.mW[2, 1] = Wxx * (1 - self.G2) * self.G4
+            self.mW[2, 2] = Wxx * (1 - self.G2) * (1 - self.G4)
 
             self.mP[1, 2] = (self.mW[1, 2] / self.mW[1]) if self.mW[1] > 0.0 else 0.0
             self.mP[1, 0] = 1 - self.mP[1, 1] - self.mP[1, 2]
@@ -342,8 +337,8 @@ class R1G3Model(_AbstractProbability):
             self.mP[2, 2] = (self.mW[2, 2] / self.mW[2]) if self.mW[2] > 0.0 else 0.0
             self.mP[2, 0] = 1 - self.mP[2, 1] - self.mP[2, 2]
 
-            self.mP[0, 1] = ((self.mW[1] - self.mW[1, 1] - self.mW[2, 1]) / self.mW[0]) if self.mW[0] > 0.0 else 0.0
-            self.mP[0, 2] = ((self.mW[2] - self.mW[1, 2] - self.mW[2, 2]) / self.mW[0]) if self.mW[0] > 0.0 else 0.0
+            self.mP[0, 1] = (self.mW[1] - self.mW[1, 1] - self.mW[2, 1]) * W0inv
+            self.mP[0, 2] = (self.mW[2] - self.mW[1, 2] - self.mW[2, 2]) * W0inv
 
             if self.mW[0] > 0.5:
                 self.mP[0, 0] = 1 - self.mP[0, 1] - self.mP[0, 2]
@@ -370,7 +365,8 @@ class R1G4Model(_AbstractProbability):
         
             \begin{align*}
                 & W_1
-                & P_{11} (W_1 < 0,5)\text{ or }P_{22} (W_1 > 0,5) \\
+                & P_{11} (W_1 < 0,5)\text{ or }P_{xx} (W_1 > 0,5) 
+                with P_{xx} = \frac {W_{22} + W_{23} + W_{24} + W_{32} + W_{33} + W_{34} + W_{42} + W_{43} + W_{44}}{W_2 + W_3 + W_4} \\
                 & R_2 = \frac{ W_2 }{W_2 + W_3 + W_4}
                 & R_3 = \frac{ W_3 }{W_3 + W_4} \\
                 & G_2 = \frac{W_{22} + W_{23} + W_{24}}{\sum_{i=2}^{4}\sum_{j=2}^4{W_{ij}}}
@@ -389,13 +385,6 @@ class R1G4Model(_AbstractProbability):
         :nowrap:
         
         \begin{align*}
-            & \text{Calculate the 'inverted' ratios of $G_1$ and $G_11$ as follows:} \\
-            & \quad G_i^{\text{-1}} =
-            \begin{cases}
-                G^{-1} - 1.0, & \text{if } G > 0 \\
-                0,              & \text{otherwise}
-            \end{cases} \quad \forall G \in \left\{ {G_1, G_{11}}\right\} \\
-            & \\
             & \text{Calculate the base weight fractions of each component:} \\
             & W_2 = (1 - W_1) \cdot R_1 \\
             & W_3 = (1 - W_1 - W_2) \cdot R_2 \\
@@ -403,77 +392,36 @@ class R1G4Model(_AbstractProbability):
             & \\
             & \text{if $W_1 \leq 0.5$:} \\
             & \quad \text{$P_{11}$ is given}\\
-            & \quad W_{11} = W_1 \cdot P_{11} \\
-            & \quad P_{22} =
-            \begin{dcases}
-                G_2 \cdot G_3 \cdot (W_{11} - 2 \cdot W_1 + 1), & \text{if $W_2 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & \\
+            & \quad W_{xx} = W_{22} + W_{23} + W_{24} + W_{32} + W_{33} + W_{34} + W_{42} + W_{43} + W_{44} = W_1 \cdot (1 - P_{11}) + W_2 + W_3 + W_4 \\
             & \text{if $W_1 > 0.5$:} \\
-            & \quad \text{$P_{22}$ is given and $P_{11}$ is derived further down} \\ 
+            & \quad \text{$P_{xx}$ is given and $P_{11}$ is derived further down} \\
+            & \quad W_{xx} = W_{22} + W_{23} + W_{24} + W_{32} + W_{33} + W_{34} + W_{42} + W_{43} + W_{44} = P_{xx} \cdot (W_2 + W_3 + W_4) \\  
             & \\
-            & W_{22} = P_{22} \cdot W_2 \\
-            & W_{23} = W_{22} \cdot G_{11}^{-1} \cdot G_{12} \\
-            & W_{24} = W_{22} \cdot G_{11}^{-1} \cdot (1 - G_{12}) \\ 
             & \text{Caclulate a partial sum of the $2^{nd}$ component's contributions: } \\
-            & S_{2i} = W_{22} + W_{23} + W_{24} \\
-            & \\
+            & W_{2x} = W_{xx} \cdot G_2 \\
+            & \text{Calculate a partial sum of the $3^{d}$ and $4^{th}$ component's contributions:} \\
+            & W_{yx} = W_{xx} - W_{2x} \\
             & \text{Calculate a partial sum of the $3^{d}$ component's contributions:} \\
-            & S_{3i} = G_1^{-1} \cdot G_2 \cdot S_{2i} \\
-            & W_{32} = S_{3i} \cdot G_{32} \\
-            & W_{33} = (S_{3i} - W_{32}) \cdot G_{33} \\
-            & W_{34} = S_{3i} - W_{32} - W_{32} \\
-            & \\
+            & W_{3x} = W_{yx} \cdot G_3 \\
             & \text{Calculate a partial sum of the $4^{th}$ component's contributions:} \\
-            & S_{4i} = G_1^{-1} \cdot (1 - G_2) \cdot S_{2i} \\
-            & W_{42} = S_{4i} \cdot G_{42} \\ 
-            & W_{43} = (S_{4i} - W_{42}) \cdot G_{43} \\
-            & W_{44} = S_{4i} - W_{42} - W_{43} \\
+            & W_{4x} = W_{yx} - W_{3x} \\
             & \\
-            & P_{23} =
-            \begin{dcases}
-                \frac{W_{23}}{W_2}, & \text{if $W_2 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{24} =
-            \begin{dcases}
-                \frac{W_{24}}{W_2}, & \text{if $W_2 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{21} = 1 - P_{22} - P_{23} - P_{24} \\
+            & W_{22} = G_{22} \cdot W_{2x} \\
+            & W_{23} = G_{23} \cdot (W_{2x} - W_{22}) \\
+            & W_{24} = W{2x} - W_{22} - W_{23} \\
             & \\
-            & P_{32} =
-            \begin{dcases}
-                \frac{W_{32}}{W_3}, & \text{if $W_3 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{33} =
-            \begin{dcases}
-                \frac{W_{33}}{W_3}, & \text{if $W_3 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{34} =
-            \begin{dcases}
-                \frac{W_{34}}{W_3}, & \text{if $W_3 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{31} = 1 - P_{32} - P_{33} - P_{34} \\
+            & W_{32} = G_{32} \cdot W_{3x} \\
+            & W_{33} = G_{33} \cdot (W_{3x} - W_{32}) \\
+            & W_{34} = W{3x} - W_{32} - W_{33} \\
             & \\
-            & P_{42} = \begin{dcases}
-                \frac{W_{42}}{W_4}, & \text{if $W_4 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{43} = \begin{dcases}
-                \frac{W_{43}}{W_4}, & \text{if $W_4 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{44} = \begin{dcases}
-                \frac{W_{44}}{W_4}, & \text{if $W_4 > 0$} \\
-                0, & \text{otherwise}
-            \end{dcases} \\
-            & P_{41} = 1 - P_{42} - P_{43} - P_{44} \\
+            & W_{42} = G_{42} \cdot W_{4x} \\
+            & W_{43} = G_{43} \cdot (W_{4x} - W_{42}) \\
+            & W_{44} = W{4x} - W_{42} - W_{43} \\
             & \\
+            & \text{ From the above weight fractions the junction probabilities 
+                for any combination of $2^{nd}$, $3^{d}$ and $4^{th}$ type
+                components can be calculated. } \\
+            & \text{ The remaining probabilities are: } \\
             & P_{12} = \begin{dcases}
                 \frac{W_2 - W_{22} - W_{32} - W_{42}}{W_1}, & \text{if $W_1 > 0$} \\
                 0, & \text{otherwise}
@@ -489,8 +437,7 @@ class R1G4Model(_AbstractProbability):
             & \\
             & \text{if $W_1 \leq 0.5$}: \\
             & \quad P_{11} = 1 - P_{12} - P_{13} - P_{14} \\
-            & \\
-            & \text{Remainder of weight fraction can be calculated as follows:} \\
+            & \text{Remainder of weight fraction can now be calculated as follows:} \\
             & \quad W_{ij} = {W_{ii}} \cdot {P_{ij}} \quad \forall {i,j} \in \left[ {1, 4} \right] \\
         \end{align*}
     """
@@ -647,61 +594,42 @@ class R1G4Model(_AbstractProbability):
     # ------------------------------------------------------------
     def update(self):
         with self.data_changed.hold_and_emit():
-            G1inv = (1.0 / self.G1) - 1.0 if self.G1 > 0 else 0.0
-
-            G11inv = (1.0 / self.G11) - 1.0 if self.G11 > 0 else 0.0
-            # G21inv = (1.0 / self.G21) - 1.0 if self.G21 > 0 else 0.0
-            # G31inv = (1.0 / self.G31) - 1.0 if self.G31 > 0 else 0.0
-
             self.mW[0] = self.W1
             self.mW[1] = (1.0 - self.mW[0]) * self.R1
             self.mW[2] = (1.0 - self.mW[0] - self.mW[1]) * self.R2
             self.mW[3] = 1.0 - self.mW[0] - self.mW[1] - self.mW[2]
 
             W0inv = 1.0 / self.mW[0] if self.mW[0] > 0.0 else 0.0
-            W1inv = 1.0 / self.mW[1] if self.mW[1] > 0.0 else 0.0
-            W2inv = 1.0 / self.mW[1] if self.mW[1] > 0.0 else 0.0
-            W3inv = 1.0 / self.mW[1] if self.mW[1] > 0.0 else 0.0
 
             if self.mW[0] < 0.5: # P11 is given
                 self.mP[0, 0] = self.P11_or_P22
-                self.mW[0, 0] = self.mW[0] * self.mP[0, 0]
-                if (self.mW[1] * self.G1) > 0.0:
-                    self.mP[1, 1] = self.G1 * self.G2 * (self.mW[0, 0] - 2 * self.mW[0] + 1.0)
-                else:
-                    self.mP[1, 1] = 0.0
+                Wxx = self.mW[0] * (self.mP[0, 0] - 1) + self.mW[1] + self.mW[2] + self.mW[3]
             else: # P22 is given
-                self.mP[0, 0] = 0.0 # mP[0,0] (aka P11) is derived further down
-                self.mP[1, 1] = self.P11_or_P22
+                Wxx = self.P11_or_P22 * (self.mW[1] + self.mW[2] + self.mW[3])
 
-            self.mW[1, 1] = self.mP[1, 1] * self.mW[1]
-            self.mW[1, 2] = self.mW[1, 1] * G11inv * self.G12
-            self.mW[1, 3] = self.mW[1, 1] * G11inv * (1.0 - self.G12)
-            SBi = self.mW[1, 1] + self.mW[1, 2] + self.mW[1, 3]
+            W1x = Wxx * self.G1 # = W11 + W12 + W13
+            Wyx = (Wxx - W1x)   # = W21 + W22 + W23 + W31 + W32 + W33
+            W2x = Wyx * self.G2 # = W21 + W22 + W23
+            W3x = Wyx - W2x     # = W31 + W32 + W33
 
-            SCi = G1inv * self.G2 * SBi
-            self.mW[2, 1] = SCi * self.G21
-            self.mW[2, 2] = (SCi - self.mW[2, 1]) * self.G22
-            self.mW[2, 3] = SCi - self.mW[2, 1] - self.mW[2, 2]
+            self.mW[1, 1] = self.G11 * W1x
+            self.mW[1, 2] = self.G12 * (W1x - self.mW[1, 1])
+            self.mW[1, 3] = W1x - self.mW[1, 1] - self.mW[1, 2]
 
-            SDi = G1inv * (1.0 - self.G2) * SBi
-            self.mW[3, 1] = SDi * self.G31
-            self.mW[3, 2] = (SDi - self.mW[3, 1]) * self.G32
-            self.mW[3, 3] = SDi - self.mW[3, 1] - self.mW[3, 2]
+            self.mW[2, 1] = self.G21 * W2x
+            self.mW[2, 2] = self.G22 * (W2x - self.mW[2, 1])
+            self.mW[2, 3] = W2x - self.mW[2, 1] - self.mW[2, 2]
 
-            self.mP[1, 2] = self.mW[1, 2] * W1inv
-            self.mP[1, 3] = self.mW[1, 3] * W1inv
-            self.mP[1, 0] = 1 - self.mP[1, 1] - self.mP[1, 2] - self.mP[1, 3]
+            self.mW[3, 1] = self.G31 * W3x
+            self.mW[3, 2] = self.G32 * (W3x - self.mW[3, 1])
+            self.mW[3, 3] = W3x - self.mW[3, 1] - self.mW[3, 2]
 
-            self.mP[2, 1] = self.mW[2, 1] * W2inv
-            self.mP[2, 2] = self.mW[2, 2] * W2inv
-            self.mP[2, 3] = self.mW[2, 3] * W2inv
-            self.mP[2, 0] = 1 - self.mP[2, 1] - self.mP[2, 2] - self.mP[2, 3]
-
-            self.mP[3, 1] = self.mW[3, 1] * W3inv
-            self.mP[3, 2] = self.mW[3, 2] * W3inv
-            self.mP[3, 3] = self.mW[3, 3] * W3inv
-            self.mP[3, 0] = 1 - self.mP[3, 1] - self.mP[3, 2] - self.mP[3, 3]
+            for i in range(1, 4):
+                self.mP[i, 0] = 1
+                for j in range(1, 4):
+                    self.mP[i, j] = self.mW[i, j] / self.mW[i] if self.mW[i] > 0 else 0
+                    self.mP[i, 0] -= self.mP[i, j]
+                self.mW[i, 0] = self.mW[i] * self.mP[i, 0]
 
             self.mP[0, 1] = (self.mW[1] - self.mW[1, 1] - self.mW[2, 1] - self.mW[3, 1]) * W0inv
             self.mP[0, 2] = (self.mW[2] - self.mW[1, 2] - self.mW[2, 2] - self.mW[3, 2]) * W0inv

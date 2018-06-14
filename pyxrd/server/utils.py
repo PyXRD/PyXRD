c7f07207c7f258f095d64f43b15f10a51bf4c733
Mathijs Dumon <mathijs.dumon@gmail.com>
2018-06-14 11:01:16 +0200

    Bugfix

--- pyxrd/server/utils.py
+++ pyxrd/server/utils.py
@@ -8,9 +8,13 @@
 import logging
 logger = logging.getLogger(__name__)
 
-import sys, os, time, subprocess
+import sys, os, time, subprocess, platform
 import signal
 import atexit
+
+pythonexe = "python3"
+if platform.system() == "Windows":
+    pythonexe = "python3w"
 
 def kill_child(child_pid):
     if child_pid is None:
@@ -19,6 +23,8 @@ def kill_child(child_pid):
         os.kill(child_pid, signal.SIGTERM)
 
 def start_script(local_script_name, auto_kill=True, log_file=None):
+    global pythonexe
+    
     if hasattr(sys, "frozen"):
         module_path = os.path.dirname(sys.executable)
     else:
@@ -28,7 +34,7 @@ def start_script(local_script_name, auto_kill=True, log_file=None):
     logging.info("Starting server using script: '%s', logging to '%s'" % (path, log_file))
     log_file = log_file if log_file is not None else os.devnull
     with open(log_file, 'w') as output:
-        proc = subprocess.Popen(["python3w", path], stdout=output)
+        proc = subprocess.Popen([pythonexe, path], stdout=output)
 
     # Register this child pid to be killed when the parent dies:
     if auto_kill:
